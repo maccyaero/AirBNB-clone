7cc9e6c00152d6a3befe7b49dc7d34a39cb78d6e
maccyaero <17176445+maccyaero@users.noreply.github.com>
2023-03-21 16:03:34 +1100

    Add Login Page Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './App.css';
 import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
 
   return (
     <Routes>
       <Route index element={<IndexPage />} />
+      <Route path='/login' element={<LoginPage />} />
     </Routes>
 
   )
 }
 
-export default App
+export default App;
