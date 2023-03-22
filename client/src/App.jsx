4094a20a080240cf13320b31eb756f68a1c0f983
maccyaero <17176445+maccyaero@users.noreply.github.com>
2023-03-22 15:25:07 +1100

    Add Layout.jsx and edit Route in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,17 @@ import ReactDOM from "react-dom";
 import './App.css';
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
+import Layout from './Layout';
 
 function App() {
 
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
-      <Route path='/login' element={<LoginPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Route>
+
     </Routes>
 
   )
